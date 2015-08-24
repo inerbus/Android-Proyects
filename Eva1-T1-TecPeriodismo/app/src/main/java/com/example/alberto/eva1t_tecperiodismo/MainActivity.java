c74ca6967a6afbcdb7e41e5a2e05322dc93d3be7
Inerbus <alberto.molina1103@gmail.com>
2015-08-23 23:07:37 -0600

    Arreglando Strings

--- Eva1-T1-TecPeriodismo/app/src/main/java/com/example/alberto/eva1t_tecperiodismo/MainActivity.java
+++ Eva1-T1-TecPeriodismo/app/src/main/java/com/example/alberto/eva1t_tecperiodismo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.alberto.eva1t_tecperiodismo;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getActionBar();
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Ciclos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner=(Spinner)findViewById(R.id.spinner);
+            spinner.setAdapter(adapter);
+
     }
 
     @Override
